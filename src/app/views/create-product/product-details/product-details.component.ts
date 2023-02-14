65ccf8c8237ead33f6188ca5ea412ee9edd32355
miswah <miswah.md@gmail.com>
2023-02-14 19:16:19 +0530

    (enh): product-details component

--- src/app/views/create-product/product-details/product-details.component.ts
+++ src/app/views/create-product/product-details/product-details.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NbDateService } from "@nebular/theme";
 
 @Component({
-  selector: 'ngx-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  selector: "ngx-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.scss"],
 })
 export class ProductDetailsComponent implements OnInit {
+  min: Date;
+  max: Date;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(protected dateService: NbDateService<Date>) {
+    this.min = this.dateService.addMonth(this.dateService.today(), -1);
+    this.max = this.dateService.addMonth(this.dateService.today(), 1);
   }
 
+  ngOnInit(): void {}
 }
