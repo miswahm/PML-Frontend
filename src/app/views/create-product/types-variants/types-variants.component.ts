f23e4f3505f901664c164d7b0ad62dc3958873fb
miswah <miswah.md@gmail.com>
2023-02-15 17:35:04 +0530

    (enh): sum Insured

--- src/app/views/create-product/types-variants/types-variants.component.ts
+++ src/app/views/create-product/types-variants/types-variants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormArray, FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "ngx-types-variants",
@@ -7,11 +8,66 @@ import { Component, OnInit } from "@angular/core";
 })
 export class TypesVariantsComponent implements OnInit {
   selectedItem = [1, 2];
-  constructor() {}
 
-  ngOnInit(): void {}
+  variantName: FormGroup;
+  baseSum: FormGroup;
+
+  variantsName = [];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.variantName = this.fb.group({
+      variants: this.fb.array([]),
+    });
+
+    this.baseSum = this.fb.group({
+      sum: this.fb.array([]),
+    });
+
+    this.addVariantName();
+    this.addBaseSum();
+  }
 
   compareById(v1, v2): boolean {
     return v1 == v2;
+  }
+
+  get variants(): FormArray {
+    return this.variantName.get("variants") as FormArray;
+  }
+
+  get sum(): FormArray {
+    return this.baseSum.get("sum") as FormArray;
+  }
+
+  newVariant(body): FormGroup {
+    return this.fb.group({
+      name: body.name,
+      id: this.variantName.value.variants.length,
+    });
+  }
+
+  newBaseSum(body): FormGroup {
+    return this.fb.group({
+      title: body.title,
+      variantId: body.variantId,
+    });
+  }
+
+  addVariantName() {
+    this.variants.push(this.newVariant({ name: "" }));
+  }
+
+  addBaseSum() {
+    this.sum.push(this.newBaseSum({ title: null, variantId: null }));
+  }
+
+  removeVariant(i: number) {
+    this.variants.removeAt(i);
+  }
+
+  removeSum(i: number) {
+    this.sum.removeAt(i);
   }
 }
